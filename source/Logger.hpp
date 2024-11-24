4d73ca429f22abed3e51cebc79d9d383a8f25d5e
Artem Leshchukov <artemleshchukov02@gmail.com>
2024-11-24 20:30:02 +0300

    Wrote an algorithm on how to integrate ELK stack into the project

---
+++ source/Logger.hpp
@@ -0,0 +1,8 @@
+/*	Algorithm:
+*	1) Update docker-compose.yml with Elasticsearch, Logstash, Kibana
+*	2) Configure logstash.conf
+*	3) Write a logger class (singleton) using spdlog and nlohmann libs
+*	4) Add code to C++ components to write logs for different events
+*/
+
+
